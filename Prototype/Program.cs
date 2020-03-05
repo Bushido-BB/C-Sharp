6f1ab35f1ffa734f7658dd73e96a142477f8059f
Bushido-BB <nikhil.mhy97@gmail.com>
2020-03-05 06:14:43 +0530

    first commit

---
+++ Prototype/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] cars = {"Bugatti", "Saleen", "Pagani"};
+            Console.WriteLine("Hello " + showName());
+        }
+        static string showName()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
